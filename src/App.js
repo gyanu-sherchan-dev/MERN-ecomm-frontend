e04d5af03b3f4a6793e948cfdc771bf4eb812968
Gyanu <gyanusherchan@outlook.com>
2023-06-15 18:22:33 +1000

    added cartPage with link to homepage

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import "./App.css";
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
-
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
   Link,
 } from "react-router-dom";
+import CartPage from "./pages/CartPage";
 
 const router = createBrowserRouter([
   {
@@ -23,6 +23,11 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <SignUpPage />,
+  },
+  {
+    //only for testing- then we will add page later
+    path: "/cart",
+    element: <CartPage />,
   },
 ]);
 
