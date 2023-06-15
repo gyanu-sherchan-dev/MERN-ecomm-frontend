eeeb70806ed16d5718423007c83c9fa40a8b16c4
Gyanu <gyanusherchan@outlook.com>
2023-06-15 17:18:38 +1000

    basic login and signIn page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
+// import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <LoginPage /> */}
+      <SignUpPage />
     </div>
   );
 }
