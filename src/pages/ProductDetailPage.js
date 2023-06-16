3d46bcc471f69d9d9499216b8f5aaa20ee542685
Gyanu <gyanusherchan@outlook.com>
2023-06-16 17:53:47 +1000

    added: product detail page

--- src/pages/ProductDetailPage.js
+++ src/pages/ProductDetailPage.js
@@ -1,5 +1,5 @@
 import Navbar from "../features/navbar/Navbar";
-import ProudctDetail from "../features/product-list/components/ProductDetail";
+import ProudctDetail from "../features/product/components/ProductDetail";
 
 function ProductDetailPage() {
   return (
